1079a9631c5534031afbcd9df6fc0d0878bea6d8
kozazz <kozazz@hanmail.net>
2015-06-22 15:41:37 +0900

    init project

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # DubuFrameWork
+개발 코드를 블럭화하여 재사용을 쉽게하는데 목적이 있다.
+- simple web 프로젝트로 maven 프로젝트 생성
+- pom.xml 에 dependency 만 추가
+
+test 쪽에만 코드를 붙인다. gradle 도 언젠가 해야 하는디.. ;;
+
+
+내용
+확작 유틸 joda
+유틸 goole 구와바
+유틸 velocity utils
+유틸 자바 기본 샘플 요런거 테스트 용 https://docs.oracle.com/javase/tutorial/java/javaOO/enum.html
+스프링 spring-framework 4.1 jdk8
+
+
+
---
+++ src/test/java/com/dubu/JavaCoreTest.java
@@ -0,0 +1,19 @@
+package com.dubu;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by rigel on 6/22/15.
+ */
+public class JavaCoreTest {
+
+    protected Logger logger = LoggerFactory.getLogger(getClass());
+
+
+    @Test
+    public void networkTest(){
+        logger.info("AAAAAAAAAAAAAAAAAAAAAAAAAAA");
+    }
+}
