c8477eaf6114cf6cf08fa8e88c09906ded9542c5
kozazz <kozazz@hanmail.net>
2015-06-30 17:21:03 +0900

    static util 추가

---
+++ src/test/java/com/dubu/CommonUtil.java
@@ -0,0 +1,21 @@
+package com.dubu;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by rigel on 6/30/15.
+ */
+public class CommonUtil {
+
+
+    public static void reverse(){
+        System.out.println("AASDAD");
+    }
+
+
+    public static void sayHello(){
+        System.out.println("Hello Util ");
+
+    }
+}
--- src/test/java/com/dubu/GuavaTest.java
+++ src/test/java/com/dubu/GuavaTest.java
@@ -2,6 +2,11 @@ package com.dubu;
 
 import com.google.common.base.Optional;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static com.dubu.CommonUtil.reverse;
+import static com.dubu.CommonUtil.sayHello;
 
 /**
  * Created by rigel on 6/23/15.
@@ -9,6 +14,8 @@ import org.junit.Test;
  * guava 샘플을 다 합쳐 봅시다.
  */
 public class GuavaTest {
+
+    protected Logger logger = LoggerFactory.getLogger(getClass());
 
     @Test
     public void g(){
@@ -18,4 +25,21 @@ public class GuavaTest {
         possible.isPresent();
         possible.get();
     }
+
+
+    @Test
+    public void aaa(){
+
+        boolean asdf  = false;
+
+        boolean sss  = true;
+
+        if(asdf|=sss){
+            logger.info("asfdasdf");
+
+        }
+        sayHello();
+    }
+
+
 }
